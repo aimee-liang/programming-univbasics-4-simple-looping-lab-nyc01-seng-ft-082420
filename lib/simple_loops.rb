cd184a72cf47d7b09097b376361a8e7d91b47428
Aimee Liang <aimeeeliang@gmail.com>
2020-08-10 00:38:44 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -26,7 +26,7 @@ def return_string_array(array)
   counter = 0 
   inside_array = []
   while counter < array.length do
-    new_array.push(array[counter].to_s)
+    inside_array.push(array[counter].to_s)
     counter += 1 
   end
   inside_array
