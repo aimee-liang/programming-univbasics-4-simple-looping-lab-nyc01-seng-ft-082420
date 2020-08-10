ac0abcf33ce12db9b10cdc6ac78fdb7afe092532
Aimee Liang <aimeeeliang@gmail.com>
2020-08-10 00:28:10 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -2,10 +2,11 @@ def loop_message_five_times(string)
   counter = 0
   while counter = 0 do 
     puts string
-    counter +1
+    counter += 1
 end
 
-def loop_message_n_times
+def loop_message_n_times(string, integer)
+  
 end
 
 def output_array
