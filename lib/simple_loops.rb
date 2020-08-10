07392b4603f6690f38023ac367ca1d2c7ff5e5a6
Aimee Liang <aimeeeliang@gmail.com>
2020-08-10 00:37:04 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,19 +1,31 @@
 def loop_message_five_times(string)
   counter = 0
-  while counter = 0 do 
+  while counter < 5 do 
     puts string
     counter += 1
+  end
 end
 
 def loop_message_n_times(string, integer)
-  
+  count = 0 
+  while count < integer do
+    puts string
+    count += 1
+  end  
 end
 
 def output_array(array)
   counter = 0 
-  while array[counter] do
-    puts array
+  while counter < array.length do
+    puts array[counter]
+    counter += 1
+  end
 end
 
-def return_string_array
+def return_string_array(array)
+  counter = 0 
+  while counter < array.length do
+    array[count].to_s 
+    count += 1 
+  end
 end
